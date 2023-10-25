ae8d1814d5449d89cfefdc24175b67be0185313c
FraBona <francescobonafede08@gmail.com>
2023-10-25 15:04:28 +0200

    milestone 1

--- js/app.js
+++ js/app.js
@@ -3,10 +3,23 @@ const createApp = Vue.createApp;
 createApp({
   data(){
     return{
-
+      currentIndex: 0,
+      todos: [
+        {
+          text: 'Fare i compiti',
+          done: false
+        },
+        {
+          text: 'Fare la spesa',
+          done: true
+        },
+        {
+          text: 'Fare il bucato',
+          done: false
+        }
+      ]
     }
   },
   mounted() {
-    console.log('ciao')
   }
 }).mount('#app');
